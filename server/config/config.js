7dfdd346274690dd02539838fe0e68990717c6a3
TrevorHawkins13 <trevorhawkinsxiii@gmail.com>
2015-11-24 19:52:43 -0700

    Changes

--- server/config/config.js
+++ server/config/config.js
@@ -4,31 +4,22 @@ module.exports = {
     development: {
         db: 'mongodb://localhost/capstone',
         rootPath: rootPath,
-        port: process.env.PORT || 3030,
-        facebookAuth: {
-        clientID      : '1621174691482121', 
-        clientSecret  : 'e3bd24d178cc83aff8aded58478e413b', 
-        callbackURL   : 'http://localhost:3030/auth/facebook/callback'
-    },
-    googleAuth : {
-        clientID     : '382954964029-pj4p6t3fqjfj37f4fpi9ddebbo9r8jjp.apps.googleusercontent.com',
-        clientSecret  : '_AMopx8BDOIj0lA_1_lE9Cjr',
-        callbackURL   : 'http://localhost:3030/auth/google/callback'
-    }
+        port: process.env.PORT || 3030
     },
     production: {
         rootPath: rootPath,
         db: 'mongodb://thawkins:capstone@ds057254.mongolab.com:57254/capstone',
-        port: process.env.PORT || 3030,
-        facebookAuth : {
-            clientID      : '959587974114904', 
-            clientSecret  : '13b7a6af6e1a792c4bf79deb2f181ce9', 
-            callbackURL   : 'http://singlepoint.herokuapp.com/auth/facebook/callback'
-        },
-        googleAuth : {
-            clientID      : '382954964029-pj4p6t3fqjfj37f4fpi9ddebbo9r8jjp.apps.googleusercontent.com',
-            clientSecret  : '_AMopx8BDOIj0lA_1_lE9Cjr',
-            callbackURL   : 'http://singlepoint.herokuapp.com/auth/google/callback'
-        }
+        port: process.env.PORT || 3030
+        
+    },
+    'facebookAuth' : {
+        'clientID'      : '1621174691482121', 
+        'clientSecret'  : 'e3bd24d178cc83aff8aded58478e413b', 
+        'callbackURL'   : 'http://localhost:3030/auth/facebook/callback'
+    },
+    'googleAuth' : {
+        'clientID'      : '382954964029-pj4p6t3fqjfj37f4fpi9ddebbo9r8jjp.apps.googleusercontent.com',
+        'clientSecret'  : '_AMopx8BDOIj0lA_1_lE9Cjr',
+        'callbackURL'   : 'http://localhost:3030/auth/google/callback'
     }
 };
