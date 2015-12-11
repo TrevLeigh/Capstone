c2b2afbf172ff2210cc8daed9ef9119ec84f9942
TrevorHawkins13 <trevorhawkinsxiii@gmail.com>
2015-12-11 01:32:31 -0700

    Change

--- server/config/routes.js
+++ server/config/routes.js
@@ -21,7 +21,7 @@ module.exports = function(app){
     
     app.get('/auth/google', passport.authenticate('google',{scope: ['profile','email']}));
     
-    app.get('/auth/google/callback',
+    app.get('/oauth2callback',
         passport.authenticate('google', {
             successRedirect : '/#/home',
             failureRedirect : '/#/login'
